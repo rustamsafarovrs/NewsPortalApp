ea7f21f401d3fa4b762cc38623ec032ac5dc2024
rustamsafarovrs <rs.programmer@hotmail.com>
2019-02-28 15:40:30 +0500

    Added GradientDrawable to News Item

--- app/src/main/java/devteam/rs/newsportaltest/Adapter.java
+++ app/src/main/java/devteam/rs/newsportaltest/Adapter.java
@@ -2,6 +2,8 @@ package devteam.rs.newsportaltest;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +14,12 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.Random;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
@@ -58,14 +62,25 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(mContext, pid, Toast.LENGTH_SHORT).show();
-
                 Intent intent = new Intent(mContext, NewsDetailActivity.class);
                 intent.putExtra(NewsDetailActivity.NEWS_ID, pid);
                 mContext.startActivity(intent);
 
             }
         });
+
+        Random rnd = new Random();
+
+        GradientDrawable gd = new GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                new int[] {0xFFFFFFFF,
+                Color.argb(50,
+                        rnd.nextInt(255),
+                        rnd.nextInt(255),
+                        rnd.nextInt(255))});
+        gd.setCornerRadius(0f);
+
+        holder.layout.setBackground(gd);
 
     }
 
@@ -83,6 +98,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         TextView title;
         TextView subtitle;
         CardView cardView;
+        View layout;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -92,6 +108,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             title = itemView.findViewById(R.id.news_item_title);
             subtitle = itemView.findViewById(R.id.news_item_subtitle);
             cardView = itemView.findViewById(R.id.news_item_card_view);
+            layout = itemView.findViewById(R.id.main_layout);
         }
     }
 }
